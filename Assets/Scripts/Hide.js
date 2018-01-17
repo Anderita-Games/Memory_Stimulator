1480fe037adff668aa73c679e2028038b699fc1c
Andrew Friedman <andrewfriedman01@gmail.com>
2018-01-16 20:06:32 -0500

    Added App
    This is the first version of Memory Stimulator that GitHub has ever seen!

---
+++ Assets/Scripts/Hide.js
@@ -0,0 +1,13 @@
+﻿#pragma strict
+
+function Start () {
+
+}
+
+function Update () {
+	if (PlayerPrefs.GetInt("hide") == 1) {
+		GetComponent.<Renderer>().enabled = false;
+	}else if (PlayerPrefs.GetInt("hide") == 0) {
+		GetComponent.<Renderer>().enabled = true;
+	}
+}
---
+++ Assets/Scripts/Shaders.js
@@ -0,0 +1,13 @@
+﻿#pragma strict
+
+function Start () {
+	PlayerPrefs.SetInt("Endpoint", 0);
+	Time.timeScale = PlayerPrefs.GetInt("paused");
+}
+
+function OnCollisionStay(col : Collision) {
+	
+	if (gameObject.name == "Endpoint") {
+			PlayerPrefs.SetInt(gameObject.name, 1);
+	}
+}	
